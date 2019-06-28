fa3926ff902252ccb7e33caaca31c1db0e4df4cd
RomanistHere <RomanistHere@gmail.com>
2019-06-28 23:15:42 +0300

    rework one settimeout with mutationObeserver. Not tested

--- linkedOn.js
+++ linkedOn.js
@@ -3,27 +3,49 @@
 var enabled = true;
 
 function run() {
-    console.log(enabled)
+    // console.log(enabled)
     if (!enabled) return false
 
-    let elements = document.querySelectorAll(".search-result__action-button")
+    // let elements = document.querySelectorAll(".search-result__action-button")
 
-    for (let i=0; i < elements.length; i++) {
+    // for (let i=0; i < elements.length; i++) {
 
-        let element = elements[i]
+    //     let element = elements[i]
 
-        element.click()
+    //     element.click()
 
-        if (document.querySelector('.artdeco-button--3.ml1')) {
-            document.querySelector('.artdeco-button--3.ml1').click()
-            setTimeout(function() {
-                document.querySelector('.artdeco-button--3.ml1').click()
-                document.querySelector('.artdeco-pagination__button--next').click()
-            }, 1000);
-        }
+    //     if (document.querySelector('.artdeco-button--3.ml1')) {
+    //         document.querySelector('.artdeco-button--3.ml1').click()
+    //         setTimeout(function() {
+    //             document.querySelector('.artdeco-button--3.ml1').click()
+    //             document.querySelector('.artdeco-pagination__button--next').click()
+    //         }, 1000);
+    //     }
         
-    }
-    setTimeout(run, 7000);
+    // }
+
+    // document.querySelector('.artdeco-pagination__button--next').click()
+    console.log('uploaded')
 }
 
-run()
+observeSearchCont()
+
+function observeSearchCont() {
+    let searchCont = document.querySelector('.authentication-outlet').children[0]
+
+    let dom_observer = new MutationObserver(function(mutation) {
+        mutation.forEach(function(mutation) {
+            if (mutation.oldValue) {
+                setTimeout(run, 1000);
+            }
+        })
+    })
+
+    dom_observer.observe(searchCont, { 
+        attributes: true,
+        attributeFilter: ['class'],
+        attributeOldValue: true,
+    })
+
+    run()
+}
